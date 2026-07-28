bb45f57b3bc0b02dcc9d2051429317b59ff1fa6a
fv14vimal <fv14vimal@gmail.com>
2026-07-28 11:26:54 +0900

    UIManager Updated
    Florance Day 02 Before Lunch

--- Assets/2.Florance/Scripts/UIManager.cs
+++ Assets/2.Florance/Scripts/UIManager.cs
@@ -1,6 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManager
+public class UIManager: MonoBehaviour
 {
-    
+    [SerializeField] GameObject MainMenuPanel,GamePanel,EndingPanel;
+    [SerializeField] Button PlayButton, EndButton, Restart;
+    void Start()
+    {
+       Mainmenu();
+
+    }
+
+    private void OnEnable()
+    {
+        PlayButton.onClick.AddListener(() => Game());
+    }
+
+    private void OnDisable()
+    {
+        PlayButton.onClick.RemoveListener(() => Game());
+    }
+    public void Mainmenu()
+    {
+        MainMenuPanel.SetActive(true);
+        GamePanel.SetActive(false);
+        EndingPanel.SetActive(false);
+    }
+    public void Game()
+    {
+        MainMenuPanel.SetActive(false);
+        GamePanel.SetActive(true);
+        EndingPanel.SetActive(false);
+    }
+    public void Ending()
+    {
+        MainMenuPanel.SetActive(false);
+        GamePanel.SetActive(false);
+        EndingPanel.SetActive(true);
+    }
+   
 }
