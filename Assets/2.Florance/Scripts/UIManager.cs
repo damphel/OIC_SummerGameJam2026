f8c28224b5a63a9b69ba04c3452d0085e74756f5
fv14vimal <fv14vimal@gmail.com>
2026-07-28 12:11:13 +0900

    UIManager Update
    Day 02 Florance Until Lunch

--- Assets/2.Florance/Scripts/UIManager.cs
+++ Assets/2.Florance/Scripts/UIManager.cs
@@ -3,37 +3,79 @@ using UnityEngine.UI;
 
 public class UIManager: MonoBehaviour
 {
+    #region Singleton
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                Debug.LogWarningFormat("Accesing {0} before its Awake phase", typeof(UIManager).Name);
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
     [SerializeField] GameObject MainMenuPanel,GamePanel,EndingPanel;
-    [SerializeField] Button PlayButton, EndButton, Restart;
+    [SerializeField] Button playButton, endButton, restartButton;
+    [SerializeField] ActionButton actionButton;
+
+    public ActionButton ActionButton { get => actionButton;}
+
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this || FindObjectsByType<UIManager>(FindObjectsSortMode.InstanceID).Length > 1)
+        {
+            Debug.LogWarningFormat("Please make sure there is only one {0} in the scene", typeof(UIManager).Name);
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
     void Start()
     {
-       Mainmenu();
+       ChangeToMainmenu();
 
     }
 
     private void OnEnable()
     {
-        PlayButton.onClick.AddListener(() => Game());
+        if (playButton) playButton.onClick.AddListener(ChangeToGame);
+        if (endButton) endButton.onClick.AddListener(ChangeToEnding);
+        if (restartButton) restartButton.onClick.AddListener(ChangeToMainmenu);
     }
 
     private void OnDisable()
     {
-        PlayButton.onClick.RemoveListener(() => Game());
+        if (playButton) playButton.onClick.RemoveListener(ChangeToGame);
+        if (endButton) endButton.onClick.RemoveListener(ChangeToEnding);
+        if (restartButton) restartButton.onClick.RemoveListener(ChangeToMainmenu);
     }
-    public void Mainmenu()
+    public void ChangeToMainmenu()
     {
+        Debug.Log("ChangeToMainmenu");
         MainMenuPanel.SetActive(true);
         GamePanel.SetActive(false);
         EndingPanel.SetActive(false);
     }
-    public void Game()
+    public void ChangeToGame()
     {
+        Debug.Log("ChangeToGame");
         MainMenuPanel.SetActive(false);
         GamePanel.SetActive(true);
         EndingPanel.SetActive(false);
     }
-    public void Ending()
+    public void ChangeToEnding()
     {
+        Debug.Log("ChangeToEnding");
         MainMenuPanel.SetActive(false);
         GamePanel.SetActive(false);
         EndingPanel.SetActive(true);
